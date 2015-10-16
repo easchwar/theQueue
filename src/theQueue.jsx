61c1bd4957e989ee0cae13fe3ab36a4472993ebd
easchwar <easchwarzenbach@gmail.com>
2015-10-15 18:18:50 -0700

    basic functionality

---
+++ src/theQueue.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Queue = require('./components/queue.jsx');
+
+ReactDOM.render(<Queue />, document.querySelector('#content'));
